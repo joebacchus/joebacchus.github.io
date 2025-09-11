b6293473e33da53d184b73c970a8b67a2691dc66
Joe Bacchus George <joebacchusgeorge@gmail.com>
2025-09-11 23:07:12 +0100

    caching issue

--- public/intense.js
+++ public/intense.js
@@ -1,11 +1,19 @@
+document.body.style.backgroundColor = '';
+
 const glowButton = document.querySelector('#name .glow span');
 
 glowButton.parentElement.addEventListener('mouseenter', () => {
-    document.body.style.backgroundColor = '#1a1a1a'; 
+    document.body.style.backgroundColor = '#1a1a1a';
     glowButton.textContent = 'Vanity';
 });
 
 glowButton.parentElement.addEventListener('mouseleave', () => {
+    document.body.style.backgroundColor = '';
+    glowButton.textContent = 'Joe Bacchus George';
+});
+
+// Reset background when coming back to page (mobile back button)
+window.addEventListener('pageshow', () => {
     document.body.style.backgroundColor = '';
     glowButton.textContent = 'Joe Bacchus George';
 });
